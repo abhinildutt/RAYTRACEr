f8f513a9d7a9b6750fda906899323c842a39159f
abhinildutt <abhinil.dutt@gmail.com>
2023-11-22 02:16:30 -0600

    shadows work

--- raytracer.py
+++ raytracer.py
@@ -2,6 +2,7 @@ from PIL import Image
 import math
 import numpy as np
 from sys import argv
+from collections import defaultdict
 
 #file related data
 file = open(argv[1], "r")
@@ -38,6 +39,11 @@ def magsq(x, y, z):
     return x**2 + y**2 + z**2
 
 def ray_sphere_intersection(x, y, z, radius, direction):
+    mag = math.sqrt(magsq(direction[0], direction[1], direction[2]))
+    direction[0] = direction[0] / mag
+    direction[1] = direction[1] / mag
+    direction[2] = direction[2] / mag
+
     inside = 1 if (distsq(x, y, z, ray["center"][0], ray["center"][1], ray["center"][2])) < radius**2 else 0
     tc = (x - ray["center"][0])*direction[0] + (y - ray["center"][1])*direction[1] + (z - ray["center"][2])*direction[2]
     if (not inside and tc < 0):
@@ -60,7 +66,7 @@ def calc_color(x, y, cent, rsi, obj_color, direction):
         light_dir_array = light_dir_array/ np.sqrt(np.sum(light_dir_array**2))
         light_color_array = np.array(light[1])
         object_times_light = np.multiply(light_color_array, obj_color_array)
-        p = np.array(rsi[1])
+        p = np.array(rsi)
         center = np.array(cent)
         normal = p - center
         light_dir = light_dir_array
@@ -82,6 +88,7 @@ def calc_color(x, y, cent, rsi, obj_color, direction):
 
 
 def draw():
+    intersect_to_xy = defaultdict(None)
     for x in range(Width):
         for y in range(Height):
             sx = (2*x - Width)/ max(Width, Height)
@@ -89,18 +96,42 @@ def draw():
             direction = [ray["forward"][0] + sx*ray["right"][0] + sy*ray["up"][0], 
                          ray["forward"][1] + sx*ray["right"][1] + sy*ray["up"][1], 
                          ray["forward"][2] + sx*ray["right"][2] + sy*ray["up"][2]]
-            mag = math.sqrt(magsq(direction[0], direction[1], direction[2]))
-            direction[0] = direction[0] / mag
-            direction[1] = direction[1] / mag
-            direction[2] = direction[2] / mag
-
+            
             t_min = math.inf
+            intersection_point = None
+            intersection_data = None
+
             for sphere in Spheres:
                 rsi = ray_sphere_intersection(sphere[0], sphere[1], sphere[2], sphere[3], direction)        
                 if(rsi != "no intersection" and rsi[0] < t_min):
-                    color = calc_color(x, y, [sphere[0], sphere[1], sphere[2]], rsi, sphere[4], direction)
-                    image.im.putpixel((x, y), (int(color[0]*255), int(color[1]*255), int(color[2]*255), 255))
                     t_min = rsi[0]
+                    intersection_point = rsi[1]
+                    intersection_data = [x, y, sphere, direction]
+            
+            if intersection_point:
+                intersect_to_xy[tuple(intersection_point)] = intersection_data
+ 
+
+    for points in intersect_to_xy.keys():
+        ixy = intersect_to_xy[points]
+        color = calc_color(ixy[0], ixy[1], [ixy[2][0], ixy[2][1], ixy[2][2]], points, ixy[2][4], ixy[3])
+        image.im.putpixel((ixy[0], ixy[1]), (int(color[0]*255), int(color[1]*255), int(color[2]*255), 255))
+
+    shadows(intersect_to_xy)
+
+def shadows(ixy):
+    for points in ixy.keys():
+        for light in Lights:
+            ray["center"] = list(points)
+            for sphere in Spheres:
+                if(sphere != ixy[points][2]):
+                    rsi = ray_sphere_intersection(sphere[0], sphere[1], sphere[2], sphere[3], light[0])
+                    if(rsi != "no intersection"):
+                        image.im.putpixel((ixy[points][0], ixy[points][1]), (0, 0, 0, 255))
+    ray["center"] = [0, 0, 0]
+
+        
+
 
 if __name__ == "__main__":
     commands = ['png', 'sphere', 'sun', 'color']
